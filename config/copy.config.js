7a20820620ab49cdb5fbdd3aacc58ebfa637a659
Anil Kumar <anilkumar.dhiman1@gmail.com>
2017-02-01 17:35:34 +0530

    deleted old files

--- config/copy.config.js
+++
@@ -1,6 +0,0 @@
-const copyConfig = require('../node_modules/@ionic/app-scripts/config/copy.config');
-
-copyConfig.include.push({
-  src: '{{ROOT}}/node_modules/font-awesome/fonts/',
-  dest: '{{WWW}}/assets/fonts/'
-});
